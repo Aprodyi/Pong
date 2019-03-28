3117b65be4703b14c997dc8fb15921d56839e1fe
Vladimir Fetyukov <Fetukov@rambler.ru>
2019-03-28 19:28:09 +0300

    Initial commit
    Простейшая игра Понг, с возможностью паузы и выбора скорости игры. Переход между экранами игры и паузы реализован с помощью CATransition.

---
+++ Pong/Pong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pong
+//
+//  Created by Вова on 26.03.2019.
+//  Copyright © 2019 Вова. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
